24f0da59f3ddf2a0585d0598260ffd03a0b5cb17
leonardomg888 <leonardomg888@gmail.com>
2023-01-11 04:31:18 +0100

    listo

---
+++ src/components/Error404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Error404()  {
+  return (
+     <div className="container-fluid">
+        
+     
+
+        <h1 className="h3 mb-0 text-gray-800">error pac</h1>
+        <p> seleccone otra ruta, esta es incorrecta, hagalo por la salud del gatito</p>
+        <img src="https://http.cat/404" alt="gatito"/>
+        </div>
+  )
+
+}
